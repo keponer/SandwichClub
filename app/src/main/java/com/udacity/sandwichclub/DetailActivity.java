d82966e05cd33c0bfb9f4c8f77fc50bc8c52a66a
Alejandro Roig <alroigar@upv.es>
2018-02-16 07:41:38 +0100

    Todo funcionando
    Falta acabar layout y comentar

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,23 +4,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.json.JSONException;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private ImageView ingredientsIv;
+
+    private TextView name;
+    private TextView aka;
+    private TextView origin;
+    private TextView ingredients;
+    private TextView description;
+
+    private Sandwich sandwich;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ingredientsIv = findViewById(R.id.image_iv);
+
+        name           = findViewById(R.id.name_tv);
+        aka            = findViewById(R.id.AKA_tv);
+        origin         = findViewById(R.id.origin_tv);
+        ingredients    = findViewById(R.id.ingredients_tv);
+        description    = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -36,7 +55,11 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        try {
+            sandwich = JsonUtils.parseSandwichJson(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -44,11 +67,7 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
 
-        setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -57,6 +76,30 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .into(ingredientsIv);
+
+        setTitle(sandwich.getMainName());
+
+        name.setText(sandwich.getMainName());
+
+        for(int i = 0;i<sandwich.getAlsoKnownAs().size();i++){
+
+            aka.append("- " + sandwich.getAlsoKnownAs().get(i));
+            aka.append("\n");
+        }
+
+        origin.setText(sandwich.getPlaceOfOrigin());
+
+        for(int i = 0;i<sandwich.getIngredients().size();i++){
+
+            ingredients.append("- " + sandwich.getIngredients().get(i));
+            ingredients.append("\n");
+        }
+
+        description.setText(sandwich.getDescription());
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,76 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    /**
+     * Given a json in String format, convert it in a JSONObject and create a Sandiwch Object
+     * From it.
+     * @param json JSONObject in String format
+     * @return Sandwich
+     * @throws JSONException Problems with JSON
+     */
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+
+        // Convert json in a JSONObject
+        JSONObject sandwichJSon = new JSONObject(json);
+
+        // Get the parts about names and create a new JSONObject
+        JSONObject sandwichName = sandwichJSon.getJSONObject("name");
+
+        // Get the name of the sandwich
+        String mainName = sandwichName.getString("mainName");
+
+        // Get the list of sandwich names and add it to a JSONArray
+        JSONArray sandwichAKA = sandwichName.getJSONArray("alsoKnownAs");
+
+        // Convert the sandwich names JSONArray in an List<String>
+        List<String> alsoKnowAs = getStringListFromJsonArray(sandwichAKA);
+
+        // Get the place of origin of the sandwich
+        String placeOfOrigin = sandwichJSon.getString("placeOfOrigin");
+
+        // Get the description of the sandwich
+        String description = sandwichJSon.getString("description");
+
+        // Get the image of the sandwich
+        String image = sandwichJSon.getString("image");
+
+        // Get the list of the sandwich ingredients and add it to a JSONArray
+        JSONArray sandwichIngredients = sandwichJSon.getJSONArray("ingredients");
+
+        // Convert the sandwich ingredients in a List<String>
+        List<String> ingredients = getStringListFromJsonArray(sandwichIngredients);
+
+        // Create the new sandwich
+        Sandwich sandwich = new Sandwich(mainName, alsoKnowAs, placeOfOrigin, description, image, ingredients);
+
+        return sandwich;
+    }
+
+
+    /**
+     * Given an JSONArray return a List<String>
+     * @param jArray JSONArray to convert
+     * @return new List<String>
+     * @throws JSONException Problems with JSON
+     */
+    public static List<String> getStringListFromJsonArray(JSONArray jArray) throws JSONException {
+        List<String> returnList = new ArrayList<String>();
+        for (int i = 0; i < jArray.length(); i++) {
+            String val = jArray.getString(i);
+            returnList.add(val);
+        }
+        return returnList;
     }
 }
